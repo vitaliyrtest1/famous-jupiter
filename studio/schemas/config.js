6f7d37ca293980d3db589a97f87425e6157c5326
Stackbit <projects@stackbit.com>
2021-02-03 13:32:51 +0200

    initial commit by Stackbit

---
+++ studio/schemas/config.js
@@ -0,0 +1,65 @@
+export default {
+    "type": "document",
+    "name": "config",
+    "title": "Site Configuration",
+    "fields": [
+        {
+            "type": "string",
+            "name": "title",
+            "title": "Title",
+            "description": "Site title",
+            "validation": Rule => Rule.required()
+        },
+        {
+            "type": "string",
+            "name": "description",
+            "title": "Site Description",
+            "description": "The default site description used in meta data.",
+            "validation": null
+        },
+        {
+            "type": "header",
+            "name": "header",
+            "title": "Header Configuration",
+            "validation": null
+        },
+        {
+            "type": "footer",
+            "name": "footer",
+            "title": "Footer Configuration",
+            "validation": null
+        },
+        {
+            "type": "string",
+            "name": "stackbit_file_path",
+            "title": "Config File Path",
+            "description": "The file path of the configuration file",
+            "hidden": false,
+            "validation": Rule => Rule.required(),
+            "options": {
+                "list": [
+                    "site-metadata.json"
+                ]
+            }
+        },
+        {
+            "type": "string",
+            "name": "stackbit_model_type",
+            "title": "Stackbit Model Type",
+            "description": "Stackbit model type",
+            "hidden": false,
+            "validation": Rule => Rule.required(),
+            "options": {
+                "list": [
+                    "config"
+                ]
+            }
+        }
+    ],
+    "singleInstance": true,
+    "preview": {
+        "select": {
+            "title": "title"
+        }
+    }
+}
